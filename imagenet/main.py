4735e0bcd0f65b21b9166c1f7e76e1cf7d14ae9b
Abhishek Verma <608abhishek@gmail.com>
2022-04-05 18:19:54 -0400

    only train, no test

--- imagenet/main.py
+++ imagenet/main.py
@@ -252,7 +252,7 @@ def main_worker(gpu, ngpus_per_node, args):
 
         # train for one epoch
         train(train_loader, model, criterion, optimizer, epoch, args)
-
+        continue
         # evaluate on validation set
         acc1 = validate(val_loader, model, criterion, args)
         
