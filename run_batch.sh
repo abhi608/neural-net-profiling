a1cad8afbb0941b7d83a57c910f175b54c12ae23
Abhishek Verma <608abhishek@gmail.com>
2022-04-06 14:59:06 -0400

    batch file added

--- run_batch.sh
+++ run_batch.sh
@@ -7,5 +7,5 @@
 #SBATCH --partition=n1s8-v100-1
 #SBATCH --gres=gpu:1
 
-cd neural-net-profiling
+cd ~/neural-net-profiling
 singularity exec --bind /share/apps/cuda --overlay /share/apps/datasets/imagenet/imagenet-train.sqf:ro --overlay /share/apps/datasets/imagenet/imagenet-val.sqf:ro --nv ~/nn-profile.sif ./complete_profile.sh
