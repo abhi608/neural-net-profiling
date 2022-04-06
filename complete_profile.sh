0ccee9b3065a030031a7bc63405c62cc22a285d3
Abhishek Verma <608abhishek@gmail.com>
2022-04-06 14:26:33 -0400

    bug fix

--- complete_profile.sh
+++ complete_profile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sh run_profiler.sh resnet18
-sh run_profiler.sh resnet34
-sh run_profiler.sh resnet50
+sh ~/neural-net-profiling/run_profiler.sh resnet18
+sh ~/neural-net-profiling/run_profiler.sh resnet34
+sh ~/neural-net-profiling/run_profiler.sh resnet50
