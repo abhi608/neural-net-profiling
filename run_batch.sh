1da80a87b5dc84a7e28775e8c3fd5e5099a25e52
Abhishek Verma <608abhishek@gmail.com>
2022-04-06 14:34:21 -0400

    batch file added

---
+++ run_batch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=av2783-imagenet_profiling
+#SBATCH --mail-type=END
+#SBATCH --mail-user=av2783@nyu.edu
+#SBATCH --output=slurm_%j.out
+#SBATCH --account=csci_ga_3033_085_2022sp
+#SBATCH --partition=n1s8-v100-1
+#SBATCH --gres=gpu:1
+
+cd neural-net-profiling
+singularity exec --bind /share/apps/cuda --overlay /share/apps/datasets/imagenet/imagenet-train.sqf:ro --overlay /share/apps/datasets/imagenet/imagenet-val.sqf:ro --nv ~/nn-profile.sif ./complete_profile.sh
