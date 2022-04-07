f78b747ba3b53dd776e8a2c9f87ae9465b739124
Abhishek Verma <608abhishek@gmail.com>
2022-04-07 00:30:53 -0400

    to compute theory ai

--- theory/ai.py
+++ theory/ai.py
@@ -2,6 +2,29 @@ from audioop import avg
 import torch
 from torchvision.models import resnet18, resnet34, resnet50
 
+def conv_fn(layer, x, y):
+    if type(layer) == torch.nn.modules.conv.Conv2d:
+        M = layer.in_channels
+        N = layer.out_channels
+        K = layer.kernel_size[0]
+        G = layer.groups
+        H = y.size()[2]
+        F = y.size()[3]
+        B = y.size()[0]
+        numerator = (B*F*H*M*N*K*K)/G
+        weights = (M*N*K*K)
+        memory_access_in = B*M*F*H/G
+        memory_access_out = B*N*F*H/G
+        memory_access = ((B*(M+N)*F*H)/G)
+        denominator = weights + memory_access
+        macs = B*M*N*K*K*F*H/G
+        layer.ai += (numerator/denominator)
+        layer.macs += macs
+        layer.weights += weights
+        layer.memory_access += memory_access
+        layer.memory_access_in += memory_access_in
+        layer.memory_access_out += memory_access_out
+
 handler_collection = []
 def add_hook(layer):
     if len(list(layer.children())) > 0:
