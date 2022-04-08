a76393082ec6186e8d940380f168c37fc6d6d26d
Abhishek Verma <608abhishek@gmail.com>
2022-04-08 03:28:28 -0400

    script folder added

--- complete_profile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:/share/apps/cuda/11.1.74/bin
-sh ~/neural-net-profiling/run_profiler.sh resnet18
-sh ~/neural-net-profiling/run_profiler.sh resnet34
-sh ~/neural-net-profiling/run_profiler.sh resnet50
